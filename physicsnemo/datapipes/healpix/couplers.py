4d7f5c9173266dbd676984e04dca5f23882d0cae
David Pruitt <dpruitt@nvidia.com>
2025-04-24 15:53:41 -0500

    Fix for ordering on coupler

--- physicsnemo/datapipes/healpix/couplers.py
+++ physicsnemo/datapipes/healpix/couplers.py
@@ -201,7 +201,7 @@ class ConstantCoupler:
         # create buffer for coupling
         coupled_fields = coupled_fields[
             :, :, :, self.coupled_channel_indices, :, :
-        ].permute(0, 2, 3, 1, 4, 5)
+        ].permute(2, 0, 3, 1, 4, 5)
         self.preset_coupled_fields = th.empty(
             [self.coupled_integration_dim, coupled_fields.shape[0], self.timevar_dim]
             + list(self.spatial_dims)
