0b4405d8c4b319b943513928e1105f15462df8f4
yikwill <yikwill@gmail.com>
2026-05-06 14:20:39 -0700

    Make sure native conv padding is disable correctly

--- physicsnemo/models/dlwp_healpix_layers/healpix_layers.py
+++ physicsnemo/models/dlwp_healpix_layers/healpix_layers.py
@@ -102,8 +102,8 @@ class HEALPixLayer(th.nn.Module):
 
         # Define a HEALPixPadding layer if padding is necessary
         if padding > 0:
-            # Disable native padding if it is already set
-            if "padding" in kwargs:
+            # Disable native padding for conv layers
+            if layer.__bases__[0] is th.nn.modules.conv._ConvNd:
                 kwargs["padding"] = 0
             padding_layer = make_hpx_padding_layer(
                 padding=padding,
